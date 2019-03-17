61d6e8709164d2ab1334d7e11c97fc00d54abe45
Anwesh43 <anweshthecool0@gmail.com>
2019-03-17 17:32:54 +0530

    adding drawRLCNode method

--- RotLadderCreateStage.ts
+++ RotLadderCreateStage.ts
@@ -6,6 +6,8 @@ const strokeFactor : number = 90
 const sizeFactor : number = 3
 const foreColor : string = "#388E3C"
 const backColor : string = "#212121"
+const nodes : number = 5
+const lines : number = 2
 
 const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
 
@@ -20,6 +22,29 @@ const divideScale : Function = (scale : number, i : number, n : number) : number
 const mirrorScale : Function = (scale : number, a : number, b : number) : number => {
     const k : number = scaleFactor(scale)
     return (1 - k) / a + k / b
+}
+
+const drawRLCNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = h / (nodes + 1)
+    const size : number = gap / sizeFactor
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    context.lineCap = 'round'
+    context.strokeStyle = foreColor
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.save()
+    context.translate(w / 2, 0.9 * h - gap * i)
+    for (var j = 0; j < lines; j++) {
+        context.save()
+        context.translate(-size + 2 * size * j, 0)
+        context.rotate(Math.PI / 2 * (1 - 2 * j) * divideScale(sc1, j, lines))
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(2 * size * (1 - 2 * j), 0)
+        context.stroke()
+        context.restore()
+    }
+    context.restore()
 }
 
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
